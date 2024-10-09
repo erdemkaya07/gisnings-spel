2b219ee4aba1568895452c4c5bd983e51fcfdaa5
Erdem Kaya <kerdem@live.com>
2024-10-09 11:09:33 +0200

    Gissa munner Javascript koden v2.0

--- main.js
+++ main.js
@@ -5,31 +5,32 @@ const message = document.querySelector('#message');
 const btn = document.getElementById('submit');
 
 const nummer = 33;
-
 let gissa= 0;
 const maxGissa = 10;
 
 
 gissaForm.addEventListener('submit', e => {
   e.preventDefault();
-
 gissa++;
-
-if(gissaInput.value == nummer){
-  console.log(`Bravo!  Du gissade rätt på ${gissa} försöket.`);
-} else if (gissaInput.value > nummer) {
-  console.log('Skriv ett mindre tal')
-} else if (gissaInput.value < nummer) {
-  console.log('Skriv ett större tal')
+if(gissaInput.value <= 100 && !isNaN(gissaInput.value)){
+  if(gissaInput.value == nummer){
+    message.textContent =`Bravo!  Du gissade rätt på ${gissa} försöket.`;
+  } else if (gissaInput.value > nummer) {
+    console.log('Skriv ett mindre tal')
+  } else if (gissaInput.value < nummer) {
+    console.log('Skriv ett större tal')
+  }
+} else {
+  alert('Du måste skriva mellan 1 och 100')
 }
 
 gissaCount.textContent = maxGissa - gissa;
 
 if(gissa >= maxGissa && gissaInput.value !== nummer){
-  message.textContent = `Tyvär! Nummre är ${nummer}`
+  message.textContent = `Tyvär! Nummer är ${nummer}`
   btn.disabled = true;
 }
- 
+
 })
 
 
